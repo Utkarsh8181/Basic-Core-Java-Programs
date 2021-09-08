f9034369f9ff05aaf48ff2405b435db5e0377f78
Utkarsh Mishra <utkarshkr945@gmail.com>
2021-09-09 00:03:07 +0530

    Added: Check Largest Number

---
+++ src/com/bridgelabz/LargestNumber.java
@@ -0,0 +1,28 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class LargestNumber {
+
+    public static void main(String[] args) {
+        int number1, number2, number3;
+        System.out.println("We have to check largest among these three numbers below ");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First number : ");
+        number1 = sc.nextInt();
+        System.out.print("Enter Second number : ");
+        number2 = sc.nextInt();
+        System.out.print("Enter Third number : ");
+        number3 = sc.nextInt();
+        //
+        if (number1>number2 && number1>number3){
+            System.out.println(number1 + "  is largest in " + number1 + "," + number2 + "," + number3);
+        }
+        else if (number2>number1 && number2>number3){
+            System.out.println(number2 + "  is largest in " + number1 + "," + number2 + "," + number3);
+        }
+        else {
+            System.out.println(number3 + "  is largest in " + number1 + "," + number2 + "," + number3);
+        }
+    }
+}
