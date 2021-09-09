8c2867bff67a554d1d26f96f20c19f44b9d6f28f
Utkarsh Mishra <utkarshkr945@gmail.com>
2021-09-09 20:43:41 +0530

    Added: Euclidean_Distance

--- src/com/bridgelabz/LargestNumber.java
+++ src/com/bridgelabz/LargestNumber.java
@@ -3,26 +3,17 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class LargestNumber {
-
     public static void main(String[] args) {
-        int number1, number2, number3;
-        System.out.println("We have to check largest among these three numbers below ");
+
+        System.out.println("Enter the value of x and y below to find the Euclidean distance : ");
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter First number : ");
-        number1 = sc.nextInt();
-        System.out.print("Enter Second number : ");
-        number2 = sc.nextInt();
-        System.out.print("Enter Third number : ");
-        number3 = sc.nextInt();
-        //
-        if (number1>number2 && number1>number3){
-            System.out.println(number1 + "  is largest in " + number1 + "," + number2 + "," + number3);
-        }
-        else if (number2>number1 && number2>number3){
-            System.out.println(number2 + "  is largest in " + number1 + "," + number2 + "," + number3);
-        }
-        else {
-            System.out.println(number3 + "  is largest in " + number1 + "," + number2 + "," + number3);
-        }
+        System.out.print("Enter the value of X : ");
+        int x = sc.nextInt();
+        System.out.print("Enter the value of Y : ");
+        int y = sc.nextInt();
+        //Formula to Calculate Euclidean distance from (0,0) to (X ,Y)
+        double distance = Math.sqrt((x *  x) + (y * y));
+        System.out.print("The Euclidean distance from the point (x, y) to the origin ( 0 , 0) : " + distance);
+
     }
 }
